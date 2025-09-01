e35612a6f69c80022d58054de444898ed0465059
fish <richlizardman2@gmail.com>
2025-09-01 16:02:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
 // Create a new client instance
