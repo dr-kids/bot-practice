6219fc48ff318cb64d8ab3b1074c78ca8a955394
fish <richlizardman2@gmail.com>
2025-09-01 17:21:23 -0700

    Create say.js

---
+++ commands/utility/say.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("echo")
+    .setDescription("Replies with your input!")
+    .addStringOption((option) =>
+      option
+        .setName("input")
+        .setDescription("The input to echo back")
+        .setRequired(true),
+    ),
+
+  async execute(interaction) {
+    const echo = interaction.options.getString("input") ?? "No text provided";
+
+    await interaction.reply(`${echo}`);
+  },
+};
